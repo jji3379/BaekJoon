04c06158a599a14c05b7146ae5c44ebc85ec2a58
Jeon Jun Ik <jji3379@naver.com>
2020-12-23 19:10:17 +0900

    NO_1000 A+B

---
+++ In_Output/src/test/No_1000.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Scanner;
+
+public class No_1000 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int A=scan.nextInt();
+		int B=scan.nextInt();
+		int sum=A+B;
+		System.out.println(sum);
+	}
+}
