7f02de3b2202319b51b7ec280052cdb32a06f5d5
Jeon Jun Ik <jji3379@naver.com>
2020-12-25 18:46:38 +0900

    NO_10998 A X B

---
+++ In_Output/src/test/No_10998.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Scanner;
+
+public class No_10998 {
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		int A=scan.nextInt();
+		int B=scan.nextInt();
+		int result=A*B;
+		System.out.println(result);
+	}
+}
