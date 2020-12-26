850d4999e0511e1f901b00d93f4dce375ea63fde
Jeon Jun Ik <jji3379@naver.com>
2020-12-26 16:57:58 +0900

    NO_1008 A / B

---
+++ In_Output/src/test/No_1008.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Scanner;
+
+public class No_1008 {
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		double A=scan.nextDouble();
+		double B=scan.nextDouble();
+		double result=A/B;
+		System.out.println(result);
+	}
+}
