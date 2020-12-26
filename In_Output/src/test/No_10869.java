70ef88f3fd398440863645c92f161dad12653708
Jeon Jun Ik <jji3379@naver.com>
2020-12-27 06:29:14 +0900

    NO_10869 사칙연산

---
+++ In_Output/src/test/No_10869.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.Scanner;
+
+public class No_10869 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int A=scan.nextInt();
+		int B=scan.nextInt();
+		int result1=A+B;
+		int result2=A-B;
+		int result3=A*B;
+		int result4=A/B;
+		int result5=A%B;
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result4);
+		System.out.println(result5);
+	}
+}
