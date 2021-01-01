caa8c5c0f7f341a8b83b25b0a29cbe09b8bd099b
Jeon Jun Ik <jji3379@naver.com>
2021-01-01 16:14:53 +0900

    NO_2753 윤년

---
+++ if_sentence/src/test/No_2753.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.util.Scanner;
+
+public class No_2753 {
+	public static void main(String[] args) {
+		//윤년
+		Scanner scan= new Scanner(System.in);
+		int year=scan.nextInt();
+		if(year%4==0 && year%100!=0) {
+			System.out.println("1");
+		}else if(year%4==0 && year%400==0) {
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+	}
+}
