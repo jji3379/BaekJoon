b98dbdafcfbc5556a3b8279a5bc588d2fdb5617e
Jeon Jun Ik <jji3379@naver.com>
2021-01-10 20:04:24 +0900

    NO_8393 1부터 n 까지 합

---
+++ for_sentence/src/test/No_8393.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.util.Scanner;
+
+public class No_8393 {
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		int n=scan.nextInt();
+		int sum=n*(n+1)/2;
+		System.out.println(sum);
+	}
+}
