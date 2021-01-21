9dd20578390a660a1fa4bbb6383f4f34f5445247
Jeon Jun Ik <jji3379@naver.com>
2021-01-21 23:07:55 +0900

    NO_2742 N부터 1까지 출력

---
+++ for_sentence/src/test/No_2742.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Scanner;
+
+public class No_2742 {
+	public static void main(String[] args) {
+		//기찍 N
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		for (int i = n; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+}
