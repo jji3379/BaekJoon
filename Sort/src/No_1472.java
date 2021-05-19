13c97dc2d72947ecbdb9b9c35a9510819fc06fc2
jji3379 <jji3379@naver.com>
2021-05-20 02:42:41 +0900

    NO_1472 내부 정렬(toCharArray())

---
+++ Sort/src/No_1472.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class No_1472 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        char[] arr=scan.nextLine().toCharArray();
+
+        Arrays.sort(arr);
+
+        for(int i=arr.length-1; i>=0; i--){
+            System.out.print(arr[i]);
+        }
+    }
+}
